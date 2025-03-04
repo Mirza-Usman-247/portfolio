390337dc71f629c73fef35e1596b58ebe4a0fe6f
Mirza-Usman-247 <usemanbaig2008@gmail.com>
2025-03-04 20:32:10 +0500

    api

--- src/lib/dbconnect.ts
+++ src/lib/dbconnect.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-const MONGO_URL = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID
+const MONGO_URL = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
 
 if (!MONGO_URL) {
   throw new Error("Please define the MONGODB_URI environment variable.");
@@ -8,15 +8,19 @@ if (!MONGO_URL) {
 
 export const dbconnect = async () => {
   try {
+    console.log("Connection String:", MONGO_URL);
     if (mongoose.connection.readyState === 1) {
       console.log("Already connected to MongoDB");
       return;
     }
-
-    await mongoose.connect(MONGO_URL);
+    console.log(MONGO_URL);
+    await mongoose.connect(MONGO_URL, {
+      serverSelectionTimeoutMS: 5000,
+    });
 
     console.log("Successfully connected to the MongoDB server");
   } catch (error) {
     console.error("Error while connecting to the database:", error);
+    throw error;
   }
 };
